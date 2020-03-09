934187a34c0af4d6422d16c9e9a65a07910b6c1d
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-03-09 18:19:25 +0530

    XAMARIN-33779 - Sample added.

---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/Model/City.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class City
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ DataFormXamarin/DataFormXamarin/DataFormXamarin/Model/Country.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFormXamarin
+{
+    public class Country
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
